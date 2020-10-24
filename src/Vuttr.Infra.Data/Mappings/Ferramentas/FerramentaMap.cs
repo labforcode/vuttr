7b1e29981370b71b884148dca51cd636b9b80b2e
lab4code <lab4code@LAB4CODE>
2020-10-24 17:35:36 -0300

    Adição de classes de mapeamento.

---
+++ src/Vuttr.Infra.Data/Mappings/Ferramentas/FerramentaMap.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vuttr.Domain.Entities.Ferramentas;
+
+namespace Vuttr.Infra.Data.Mappings.Ferramentas
+{
+    public class FerramentaMap : IEntityTypeConfiguration<Ferramenta>
+    {
+        public void Configure(EntityTypeBuilder<Ferramenta> builder)
+        {
+            builder.ToTable("ferramentas");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                   .HasColumnName("ferramenta_id");
+
+            builder.Property(c => c.Nome)
+                   .HasColumnName("nome")
+                   .HasColumnType("varchar(240)")
+                   .IsRequired();
+
+            builder.Property(c => c.Link)
+                   .HasColumnName("link")
+                   .HasColumnType("varchar(200)")
+                   .IsRequired();
+
+            builder.Property(c => c.Descricao)
+                   .HasColumnName("descricao")
+                   .HasColumnType("varchar(400)")
+                   .IsRequired();
+        }
+    }
+}
---
+++ src/Vuttr.Infra.Data/Mappings/Tags/TagMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Vuttr.Domain.Entities.Tags;
+
+namespace Vuttr.Infra.Data.Mappings.Tags
+{
+    public class TagMap : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("tags");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                   .HasColumnName("tag_id");
+
+            builder.Property(c => c.Nome)
+                   .HasColumnName("nome")
+                   .HasColumnType("varchar(100)")
+                   .IsRequired();
+        }
+    }
+}
