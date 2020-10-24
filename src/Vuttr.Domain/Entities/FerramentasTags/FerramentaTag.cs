2e0dd571b166bf5fed6cc499819d411ce5f8e332
lab4code <lab4code@LAB4CODE>
2020-10-24 17:34:46 -0300

    Criação de entidades.

---
+++ src/Vuttr.Domain/Entities/FerramentasTags/FerramentaTag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Vuttr.Domain.Entities.Ferramentas;
+using Vuttr.Domain.Entities.Tags;
+
+namespace Vuttr.Domain.Entities.FerramentasTags
+{
+    public class FerramentaTag
+    {
+        public FerramentaTag(Guid ferramentaId, Guid tagId)
+        {
+            FerramentaId = ferramentaId;
+            TagId = tagId;
+        }
+
+        public FerramentaTag() { }
+
+        public Guid FerramentaId { get; set; }
+
+        //EF Core
+        public virtual Ferramenta Ferramenta { get; set; }
+
+        public Guid TagId { get; set; }
+
+        //EF Core
+        public virtual Tag Tag { get; set; }
+    }
+}
--- src/Vuttr.Domain/Entities/Tags/Tag.cs
+++ src/Vuttr.Domain/Entities/Tags/Tag.cs
@@ -1,16 +1,22 @@
-﻿using Vuttr.Domain.Core.DomainObjects.Entities;
+﻿using System.Collections.Generic;
+using Vuttr.Domain.Core.DomainObjects.Entities;
+using Vuttr.Domain.Core.DomainObjects.Interfaces;
+using Vuttr.Domain.Entities.FerramentasTags;
 
 namespace Vuttr.Domain.Entities.Tags
 {
-    public class Tag : Entity
+    public class Tag : Entity, IAggregateRoot
     {
         public Tag(string nome)
         {
             Nome = nome;
         }
 
-        public Tag() { }
+        protected Tag() { }
 
         public string Nome { get; private set; }
+
+        //EF Core
+        public virtual ICollection<FerramentaTag> FerramentasTags { get; set; }
     }
 }
